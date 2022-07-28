9ecc771d1a1c24ef1acfe5767ba9bc8232ad49e1
Anna Lange <anna_lange@hotmail.de>
2022-07-28 11:13:03 +0200

    updated decision and video position

--- my_drawdecision.m
+++ my_drawdecision.m
@@ -1,13 +1,13 @@
-function my_drawdecision (w)
+function my_drawdecision (w, rect, white)
 
 % randomizing position of choices
-xpos = [300, 980]
+xpos = [rect(3)*0.25 (rect(3)-rect(3)*0.25)];
 r = randperm(2)
 pos1 = xpos(r(1));
 pos2 = xpos(r(2));
 
 
-DrawFormattedText(w, 'Human', pos1, 'center', [255 255 255]);
-DrawFormattedText(w, 'Artificial', pos2, 'center', [255 255 255]);
+DrawFormattedText(w, 'Human', pos1, 'center', white);
+DrawFormattedText(w, 'Artificial', pos2, 'center', white);
 
 end
